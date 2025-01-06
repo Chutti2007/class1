9b2fddd2814b1ca2671e314dd1e02632043df201
Chutti2007 <akimaani557@gmail.com>
2025-01-06 14:42:28 +0530

    First git commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// import chalk from "chalk";
+// const chalk = require("chalk"); // same as import
+// console.log(chalk.blue("HI I am inside class"));
+
+// import chalk from 'chalk';
+const chalk = require("chalk");
+console.log(chalk.blue.bold.inverse('Hello world!'));
+
+
+// var figlet = require("figlet");
+
+// figlet("Hello World", function(err, data){
+//     if(!err){
+//         console.log(data);
+//     }
+//     else{
+//         console.log("Something went wrong");
+//     }
+// })
